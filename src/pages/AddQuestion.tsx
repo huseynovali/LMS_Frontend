4f789b0b8bcb2781dc2615541192cc4425733d46
User <huseynovali612@gmail.com>
2024-11-11 18:08:31 +0400

    create question page

--- src/pages/AddQuestion.tsx
+++ src/pages/AddQuestion.tsx
@@ -2,7 +2,12 @@
 function AddQuestion() {
   return (
     <div className='w-full rounded-lg bg-white'>
-        
+      <div className="p-5 border-b">
+        <p className="text-lg font-semibold">Sınaq Sualı</p>
+      </div>
+        <div>
+          asd
+        </div>
     </div>
   )
 }
